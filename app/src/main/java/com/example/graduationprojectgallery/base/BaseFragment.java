36ff44976e285cda0454c76f7e8e5f59baf8a88b
wijdan <wijdanramadan1993@gmail.com>
2019-10-06 21:09:40 +0300

    navigation components with bug fragments added navigation graph added

---
+++ app/src/main/java/com/example/graduationprojectgallery/base/BaseFragment.java
@@ -0,0 +1,67 @@
+package com.example.graduationprojectgallery.base;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import com.example.graduationprojectgallery.R;
+import static androidx.navigation.fragment.NavHostFragment.findNavController;
+
+public class BaseFragment extends Fragment {
+
+    protected NavController findNavigationController() {
+        return findNavController(this);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        TextView photos = getActivity().findViewById(R.id.photos);
+        TextView foryou = getActivity().findViewById(R.id.forYou);
+        TextView albums = getActivity().findViewById(R.id.albums);
+        TextView search = getActivity().findViewById(R.id.search);
+
+        photos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (findNavController(BaseFragment.this).getCurrentDestination().getId()!= R.id.photosFragment){
+                    findNavigationController().navigate(R.id.action_mainActivityFragment_to_photosFragment);
+                }
+            }
+        });
+        // TODO: fix navigation bug :D
+
+    /*    foryou.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (findNavController(BaseFragment.this).getCurrentDestination().getId()!= R.id.foryouFragment){
+
+                    findNavigationController().navigate(R.id.action_mainActivityFragment_to_foryouFragment);
+                }
+            }
+        });
+
+        albums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (findNavController(BaseFragment.this).getCurrentDestination().getId()!= R.id.albumsFragment) {
+
+                    findNavigationController().navigate(R.id.action_mainActivityFragment_to_albumsFragment);
+                }            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (findNavController(BaseFragment.this).getCurrentDestination().getId()!= R.id.searchFragment) {
+
+                    findNavigationController().navigate(R.id.action_mainActivityFragment_to_searchFragment);
+                }            }
+        });*/
+
+
+    }
+}
---
+++ app/src/main/java/com/example/graduationprojectgallery/ui/mainactivity/MainActivityFragment.java
@@ -0,0 +1,59 @@
+package com.example.graduationprojectgallery.ui.mainactivity;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.graduationprojectgallery.R;
+import com.example.graduationprojectgallery.base.BaseFragment;
+
+import static androidx.navigation.Navigation.findNavController;
+import static com.example.graduationprojectgallery.ui.mainactivity.MainActivityFragmentDirections.actionMainActivityFragmentToPhotosFragment;
+
+public class MainActivityFragment extends BaseFragment {
+
+    private MainActivityViewModel mViewModel;
+
+    public static MainActivityFragment newInstance() {
+        return new MainActivityFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_activity_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
